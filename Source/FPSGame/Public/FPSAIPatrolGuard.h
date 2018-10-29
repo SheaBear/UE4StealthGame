2540a35cb309470f8af87f049ac03301703d64e6
SheaBear <44589295+SheaBear@users.noreply.github.com>
2018-10-29 14:35:01 -0700

    added comment for capsule overlap.

--- Source/FPSGame/Public/FPSAIPatrolGuard.h
+++ Source/FPSGame/Public/FPSAIPatrolGuard.h
@@ -25,6 +25,8 @@ public:
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaTime) override;
+	
+	// Need this to generate overlaps to send the dwarf guard to the next patrol point.
 	UFUNCTION()
 		void OnCapsuleOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
 			int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
